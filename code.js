b152635ab3cd8f3c6bbda26f942a6791d082f539
brishachu <paddlepal3@gmail.com>
2016-11-27 20:45:35 -0600

    All Matte Pages except video

--- code.js
+++ code.js
@@ -17,3 +17,32 @@ Cheeks.onclick=function(){
 Environmental.onclick=function(){
   ChangeForm(environmentForm);
 }
+lipsBack.onclick=function(){
+  ChangeForm(homeForm);
+}
+
+matteLabel.onclick=function(){
+  ChangeForm(matteForm);
+}
+matteLipsVid.onmousedown=function(){
+  
+}
+
+
+matteBack.onclick=function(){
+  ChangeForm(lipsForm);
+}
+
+colourPop.onclick=function(){
+  ChangeForm(colourPopForm);
+}
+
+stila.onclick=function(){
+  ChangeForm(stilaForm);
+}
+colourPopBack.onclick=function(){
+  ChangeForm(matteForm);
+}
+stilaBack.onclick=function(){
+  ChangeForm(matteForm);
+}
