6e8a4afec9e966a7f872fe2acdeda1d53a490b63
nida28 <nida24@live.com>
2016-12-11 18:39:02 -0600

    made a few changes
    added text to home page images, added video, added transparent buttons

---
+++ phonegap/www/code.js
@@ -0,0 +1,102 @@
+Lips.onclick=function(){
+  ChangeForm(lipsForm);
+}
+
+Skin.onclick=function(){
+  ChangeForm(skinForm);
+}
+
+Eyes.onclick=function(){
+  ChangeForm(eyesForm);
+}
+
+Cheeks.onclick=function(){
+  ChangeForm(cheeksForm);
+}
+
+Environmental.onclick=function(){
+  ChangeForm(environmentForm);
+}
+
+
+
+Label1.onmousemove=function(){
+  
+}
+lipsBack.onclick=function(){
+  ChangeForm(homeForm);
+}
+
+matteLabel.onclick=function(){
+  ChangeForm(matteForm);
+}
+
+creamLabel.onclick=function(){
+  ChangeForm(creamForm);
+}
+skinBack.onclick=function(){
+  ChangeForm(homeForm);
+}
+
+foundationLabel.onclick=function(){
+  ChangeForm(FoundationForm);
+}
+eyesBack.onclick=function(){
+  ChangeForm(homeForm);
+}
+cheeksBack.onclick=function(){
+  ChangeForm(homeForm);
+}
+envBack.onclick=function(){
+  ChangeForm(homeForm);
+}
+
+burtsLabel.onclick=function(){
+  ChangeForm(burtsForm);
+}
+matteBack.onclick=function(){
+  ChangeForm(lipsForm);
+}
+
+colourPop.onclick=function(){
+  ChangeForm(colourPopForm);
+}
+
+stila.onclick=function(){
+  ChangeForm(stilaForm);
+}
+
+PlayButton.onclick = function() {
+  matteLipsVideo.play();
+}
+colourPopBack.onclick=function(){
+  ChangeForm(matteForm);
+}
+stilaBack.onclick=function(){
+  ChangeForm(matteForm);
+}
+creamBack.onclick=function(){
+  ChangeForm(lipsForm);
+}
+
+burtsLogo.onclick = function(){
+  ChangeForm(burtsForm);
+}
+burtsBack.onclick=function(){
+  ChangeForm(creamForm);
+}
+blackUp.onclick = function() {
+    ChangeForm(blackUpForm);
+}
+
+foundBack.onclick = function() {
+    ChangeForm(skinForm);
+}
+
+
+clinique.onclick = function() {
+    ChangeForm(cliniqueForm);
+}
+blackUpBack.onclick = function() {
+    ChangeForm(FoundationForm);
+}
