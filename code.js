79f5da7a576226ab3b801b5795f1403571337f3a
brishachu <paddlepal3@gmail.com>
2016-12-12 14:03:54 -0600

    rimmel and nyx

--- code.js
+++ code.js
@@ -34,25 +34,9 @@ matteLabel.onclick=function(){
 creamLabel.onclick=function(){
   ChangeForm(creamForm);
 }
-skinBack.onclick=function(){
-  ChangeForm(homeForm);
-}
-
-foundationLabel.onclick=function(){
-  ChangeForm(FoundationForm);
-}
-eyesBack.onclick=function(){
-  ChangeForm(homeForm);
-}
-cheeksBack.onclick=function(){
-  ChangeForm(homeForm);
-}
-envBack.onclick=function(){
-  ChangeForm(homeForm);
-}
-
-burtsLabel.onclick=function(){
-  ChangeForm(burtsForm);
+
+glossLabel.onclick=function(){
+  ChangeForm(glossForm);
 }
 matteBack.onclick=function(){
   ChangeForm(lipsForm);
@@ -69,34 +53,115 @@ stila.onclick=function(){
 PlayButton.onclick = function() {
   matteLipsVideo.play();
 }
-colourPopBack.onclick=function(){
+stilaBack.onclick=function(){
   ChangeForm(matteForm);
 }
-stilaBack.onclick=function(){
+colourPopBack.onclick=function(){
   ChangeForm(matteForm);
 }
 creamBack.onclick=function(){
   ChangeForm(lipsForm);
 }
 
-burtsLogo.onclick = function(){
-  ChangeForm(burtsForm);
+burtsLogo.onclick = function(){
+  ChangeForm(burtsForm);
 }
 burtsBack.onclick=function(){
   ChangeForm(creamForm);
 }
-blackUp.onclick = function() {
-    ChangeForm(blackUpForm);
+glossBack.onclick=function(){
+  ChangeForm(lipsForm);
 }
 
-foundBack.onclick = function() {
-    ChangeForm(skinForm);
+nyxLogo.onclick = function(){
+  ChangeForm(nyxForm);
+}
+nyxBack.onclick=function(){
+  ChangeForm(glossForm);
+}
+skinBack.onclick=function(){
+  ChangeForm(homeForm);
+}
+
+foundationLabel.onclick=function(){
+  ChangeForm(FoundationForm);
+}
+
+concealerLabel.onclick=function(){
+  ChangeForm(ConcealForm);
+}
+blackUp1.onclick= function(){
+  ChangeForm(blackUpForm);
+}
+
+foundBack.onclick=function(){
+  ChangeForm(skinForm);
 }
 
 
-clinique.onclick = function() {
-    ChangeForm(cliniqueForm);
+minerals.onclick = function(){
+  ChangeForm(mineralsForm);
 }
-blackUpBack.onclick = function() {
-    ChangeForm(FoundationForm);
+
+PlayButton.onclick = function() {
+  foundVideo.play();
+}
+blackUpBack.onclick = function(){
+  ChangeForm(FoundationForm);
+}
+mineralsBack.onclick = function(){
+  ChangeForm(FoundationForm);
+}
+PlayButton.onclick = function() {
+  concealVideo.play();
+}
+
+PlayButton.onclick = function() {
+  bbCCVideo.play();
+}
+
+concealBack.onclick=function(){
+  ChangeForm(skinForm);
+}
+
+MUFE.onclick=function(){
+  ChangeForm(mufeForm);
+}
+
+ABH.onclick=function(){
+  ChangeForm(abhForm);
+}
+mufeBack.onclick=function(){
+  ChangeForm(FoundationForm);
+}
+abhBack.onclick=function(){
+  ChangeForm(FoundationForm);
+}
+eyesBack.onclick=function(){
+  ChangeForm(homeForm);
+}
+cheeksBack.onclick=function(){
+  ChangeForm(homeForm);
+}
+envBack.onclick=function(){
+  ChangeForm(homeForm);
+}
+
+burtsLabel.onclick=function(){
+  ChangeForm(burtsForm);
+}
+bbCCVideo.onresize=function(){
+  
+}
+
+bbCCBack.onclick=function(){
+  ChangeForm(skinForm);
+}
+
+marcelle.onclick=function(){
+  ChangeForm(marcelleForm);
+}
+
+iman.onclick=function(){
+  ChangeForm(imanForm);
 }
